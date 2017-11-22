6df12797ea4757ea4ed4642a2df1866052974f6a
BenJohnCarson <benjohncarson@gmail.com>
2017-11-22 10:18:46 +0000

    Stage 2 requirements complete

--- app/components/contact-form.js
+++ app/components/contact-form.js
@@ -1,4 +1,28 @@
 import Component from '@ember/component';
+import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Component.extend({
+const Validations = buildValidations({
+  email: [
+    validator('presence', true),
+    validator('format', { type: 'email' })
+  ],
+  message: [
+    validator('presence', true),
+    validator('length', { min: 5 })
+  ]
+});
+
+export default Component.extend(Validations, {
+  email: '',
+  message: '',
+  disabled: Ember.computed.not('validations.isValid'),
+
+  actions: {
+    sendMessage() {
+      this.set('email', '');
+      this.set('message', '');
+      this.set('successMessage', 'We got your message, we\'ll be in contact soon');
+    }
+  }
 });
