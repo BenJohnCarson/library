15b137407648410010ba4256234e1d8cd0e73b8c
BenJohnCarson <benjohncarson@gmail.com>
2018-02-05 15:20:25 +0000

    Moving delete responsiblity back into library entry

--- app/components/library-list.js
+++ app/components/library-list.js
@@ -1,14 +1,4 @@
 import Component from '@ember/component';
-import { task } from 'ember-concurrency';
 
 export default Component.extend({
-  deleteTask: task(function * (library) {
-    yield this.get('deleteLibrary')(library);
-  }).drop(),
-
-  actions: {
-    deleteLibrary(library) {
-      this.get('deleteTask').perform(library);
-    }
-  }
 });
